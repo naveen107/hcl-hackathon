50293890ef3857d141a66dce3ba5b03e36df06a7
Naveen V <naveen.svn107@gmail.com>
2025-08-09 19:14:35 +0530

    removed auth validation

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '../middleware/auth';
 const router = express.Router();
 
 router.post('/create',authMiddleware, createProduct);
-router.get('/list',authMiddleware, getProducts);
+router.get('/list', getProducts);
 router.get('/get',authMiddleware, getProduct);
 router.put('/updatecatgory', authMiddleware,updateProduct)
 router.delete('/deleteproduct',authMiddleware,deleteProduct)
