ac51ce6ad28bef3b7940bfc9a24c53c66f2a4aac
manikandanthalapathi03 <manikandanthalapathi03@gmail.com>
2025-08-09 19:09:29 +0530

    files front end modules added

--- frontend/ecomwebapp/src/Login.tsx
+++ frontend/ecomwebapp/src/Login.tsx
@@ -10,7 +10,6 @@ const SignUp: React.FC = () => {
         confirmPassword: '',
     });
     const [error, setError] = useState('');
-    const [loginbuttonClicked, setLoginButtonClicked] = useState(false);
     const [emailError, setEmailError] = useState('');
     const [loginbuttonClicked, setLoginButtonClicked] = useState(false);
 
